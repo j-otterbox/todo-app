7e4d80077087441cbf4441aef82228e0c353b2b2
Jason Otter <jasonotter17@gmail.com>
2022-10-03 13:05:05 -0700

    refactor class names

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-const domModule = (() => {
-  // refs
-  const addBtn = document.getElementById("addBtn");
-  const todoTxtInput = document.getElementById("todoTxt");
-  const todosList = document.getElementById("todosList");
-  const deleteBtn = document.getElementById("deleteBtn");
+const addBtn = document.querySelector(".add-btn");
+addBtn.addEventListener("click", addBtnClickHandler);
 
-  // event listeners
-  addBtn.addEventListener("click", addBtnClickHandler);
-  deleteBtn.addEventListener("click", deleteBtnClickHandler);
-  todosList.addEventListener("click", todoListClickHandler);
+const todoTxtInput = document.querySelector(".todo-txt");
 
-  // methods
-  function addBtnClickHandler(e) {
-    e.preventDefault();
-    const todoTxt = todoTxtInput.value;
+const todosList = document.querySelector(".todos-list");
+todosList.addEventListener("click", todoListClickHandler);
 
-    // check for input
-    if (todoTxt !== "") {
-      const newListItem = document.createElement("li");
-      newListItem.textContent = todoTxt;
-      todoTxtInput.value = "";
-      todosList.appendChild(newListItem);
-    }
+const deleteBtn = document.querySelector(".delete-btn");
+deleteBtn.addEventListener("click", deleteBtnClickHandler);
+
+function addBtnClickHandler(e) {
+  e.preventDefault();
+  const todoTxt = todoTxtInput.value;
+
+  // check for input
+  if (todoTxt !== "") {
+    const newListItem = document.createElement("li");
+    newListItem.textContent = todoTxt;
+    todoTxtInput.value = "";
+    todosList.appendChild(newListItem);
   }
+}
 
-  function deleteBtnClickHandler() {
-    const listItems = Array.from(todosList.querySelectorAll("li"));
-    for (const elem of listItems) {
-      if (elem.dataset.done === "true") elem.remove();
-    }
+function deleteBtnClickHandler() {
+  const listItems = Array.from(todosList.querySelectorAll("li"));
+  for (const elem of listItems) {
+    if (elem.dataset.done === "true") elem.remove();
   }
+}
 
-  function todoListClickHandler(e) {
-    if (e.target.tagName === "LI") {
-      if (!e.target.classList.contains("done")) {
-        e.target.dataset.done = "true";
-      } else e.target.dataset.done = "false";
-      e.target.classList.toggle("done");
-    }
+function todoListClickHandler(e) {
+  if (e.target.tagName === "LI") {
+    if (!e.target.classList.contains("done")) {
+      e.target.dataset.done = "true";
+    } else e.target.dataset.done = "false";
+    e.target.classList.toggle("done");
   }
-})();
+}
